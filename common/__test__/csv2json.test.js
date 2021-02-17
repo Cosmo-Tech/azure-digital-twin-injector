06bbaf87559e8a3434d3ba89e6d0908ad853d4ec
Clément Rezvoy <clement.rezvoy@gmail.com>
2021-02-17 15:30:23 +0100

    Add tests for relations

--- common/__test__/csv2json.test.js
+++ common/__test__/csv2json.test.js
@@ -33,6 +33,7 @@ describe('csv2json', () => {
             +       '"modelName","instanceOne"\n'
             +       '"modelName","instanceTwo"';
         mockMessages = [];
+        mockContext = {};
         QueueClient.mockImplementation((_, __) => {
             return {
                 createIfNotExists: jest.fn(),
@@ -54,6 +55,7 @@ describe('csv2json', () => {
         testValue = '"$metadata.$model","$id",answer\n'
             +       '"modelName","instanceId",42';
         mockMessages = [];
+        mockContext = {};
         QueueClient.mockImplementation((_, __) => {
             return {
                 createIfNotExists: jest.fn(),
@@ -72,6 +74,7 @@ describe('csv2json', () => {
         testValue = '"$metadata.$model","$id",e\n'
             +       '"modelName","instanceId",2.71828';
         mockMessages = [];
+        mockContext = {};
         QueueClient.mockImplementation((_, __) => {
             return {
                 createIfNotExists: jest.fn(),
@@ -87,9 +90,10 @@ describe('csv2json', () => {
     });
 
     it('handles object properties', async () => {
-        testValue = '"$metadata.$model","$id",eeny.meenie.miney.moe\n'
+        testValue = '"$metadata.$model","$id","eeny.meenie.miney.moe"\n'
             +       '"modelName","instanceId","catch a tiger by the toe"';
         mockMessages = [];
+        mockContext = {};
         QueueClient.mockImplementation((_, __) => {
             return {
                 createIfNotExists: jest.fn(),
@@ -103,4 +107,67 @@ describe('csv2json', () => {
         expect(mockMessages.length).toBe(1);
         expect(mockMessages[0].eeny.meenie.miney.moe).toBe('catch a tiger by the toe');
     });
+ 
+    it('handles simple relationships', async () => {
+        testValue = '"$sourceId","$targetId","$relationshipId", "$relationshipName"\n'
+            +       '"sourceId","targetId","sourceId-relation-targetId", "relationName"';
+        mockMessages = [];
+        mockContext = {};
+        QueueClient.mockImplementation((_, __) => {
+            return {
+                createIfNotExists: jest.fn(),
+                sendMessage: jest.fn().mockImplementation((str) => {
+                    mockMessages.push(JSON.parse(Buffer.from(str, 'base64').toString()));
+                    return new Promise(() => {});
+                }),
+            }
+        });
+        await csv2json(mockContext, testValue);
+        expect(mockMessages.length).toBe(1);
+        expect(mockMessages[0].$sourceId).toBe('sourceId');
+        expect(mockMessages[0].$targetId).toBe('targetId');
+        expect(mockMessages[0].$relationshipId).toBe('sourceId-relation-targetId');
+        expect(mockMessages[0].$relationshipName).toBe('relationName');
+    })
+
+    it('handles relationships with properties', async () => {
+        testValue = '"$sourceId","$targetId","$relationshipId", "$relationshipName", "property1", "nested.property"\n'
+            +       '"sourceId","targetId","sourceId-relation-targetId", "relationName", 3.14159, "value"';
+        mockMessages = [];
+        mockContext = {};
+        QueueClient.mockImplementation((_, __) => {
+            return {
+                createIfNotExists: jest.fn(),
+                sendMessage: jest.fn().mockImplementation((str) => {
+                    mockMessages.push(JSON.parse(Buffer.from(str, 'base64').toString()));
+                    return new Promise(() => {});
+                }),
+            }
+        });
+        await csv2json(mockContext, testValue);
+        expect(mockMessages.length).toBe(1);
+        expect(mockMessages[0].property1).toBe(3.14159);
+        expect(mockMessages[0].nested.property).toBe("value");
+    })
+
+    it('creates one relationship per row', async () => {
+        testValue = '"$sourceId","$targetId","$relationshipId", "$relationshipName", "property1", "nested.property"\n'
+            +       '"sourceId","targetId","sourceId-relation-targetId", "relationName", 3.14159, "value"\n'
+            +       '"sourceId2","targetId2","sourceId-relation-targetId2", "relationName2", 2.71828, "value2"';
+        mockMessages = [];
+        mockContext = {};
+        QueueClient.mockImplementation((_, __) => {
+            return {
+                createIfNotExists: jest.fn(),
+                sendMessage: jest.fn().mockImplementation((str) => {
+                    mockMessages.push(JSON.parse(Buffer.from(str, 'base64').toString()));
+                    return new Promise(() => {});
+                }),
+            }
+        });
+        await csv2json(mockContext, testValue);
+        expect(mockMessages.length).toBe(2);
+        expect(mockMessages[0].property1).toBe(3.14159);
+        expect(mockMessages[1].property1).toBe(2.71828);
+    })
 });
