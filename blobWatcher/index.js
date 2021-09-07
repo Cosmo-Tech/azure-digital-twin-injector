9bad9974e7261d7edec09458a02fa02d2c620e65
Vincent Carluer <vcarluer@gmail.com>
2021-09-07 16:44:59 +0200

    consol log + eslint

--- blobWatcher/index.js
+++ blobWatcher/index.js
@@ -2,5 +2,6 @@ const {csv2json} = require('../common/csv2json');
 
 // blob trigered call to csv2json
 module.exports = async function (context, csvBlob) {
+    console.log("blobWatcher function triggered");
     csv2json(context, csvBlob);
 };
--- common/csv2json.js
+++ common/csv2json.js
@@ -1,22 +1,36 @@
-const { QueueClient }= require('@azure/storage-queue');
+const {QueueClient} = require('@azure/storage-queue');
 const Papa = require('papaparse');
 
 
-module.exports.csv2json = async function (/*context*/ _, csvData) {
-    const queueClient = new QueueClient(process.env.JSON_STORAGE_CONNECTION, process.env.JSON_STORAGE_QUEUE);
-    queueClient.createIfNotExists();
-    const records = Papa.parse(csvData.toString('utf8'),{
-        header: true,
-        dynamicTyping: true,
-        step: function(results, parser) {
-            var content = {};
-            for (var key in results.data) {
-                key.split('.').reduce((acc, e, i, arr) => {
-                    return (i === arr.length - 1) ? (acc[e.toString()] = results.data[key]) : acc[e.toString()] || (acc[e.toString()] = {});
-                }, content);
-            }
-            queueClient.sendMessage(Buffer.from(JSON.stringify(content)).toString('base64'))
-                .catch(e => { console.log("error sending message " + e) });
-        }
-    });
+module.exports.csv2json = async function(/*context*/ _, csvData) {
+  console.log('Running csv2json...');
+  const queueClient = new QueueClient(
+      process.env.JSON_STORAGE_CONNECTION,
+      process.env.JSON_STORAGE_QUEUE);
+  console.log('Queue client: ' + process.env.JSON_STORAGE_QUEUE);
+  console.log('Queue: create if not exist');
+  queueClient.createIfNotExists();
+  console.log('Parsing CSV data...');
+  Papa.parse(csvData.toString('utf8'), {
+    header: true,
+    dynamicTyping: true,
+    step: function(results, parser) {
+      console.log('Parser step');
+      let content = {};
+      console.log('Iterating results data');
+      for (const key in results.data) {
+        key.split('.').reduce((acc, e, i, arr) => {
+          return (i === arr.length - 1) ?
+            (acc[e.toString()] = results.data[key]) :
+            acc[e.toString()] || (acc[e.toString()] = {});
+        }, content);
+      }
+      console.log('Sending message to queue');
+      queueClient.sendMessage(
+          Buffer.from(JSON.stringify(content)).toString('base64'))
+          .catch((e) => {
+            console.log('error sending message ' + e);
+          });
+    }
+  });
 };
