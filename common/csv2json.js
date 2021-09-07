f852bc6a56665433e2b1d8ac566140f726a8796e
Vincent Carluer <vcarluer@gmail.com>
2021-09-07 17:49:24 +0200

    console debug key & json transformed data

--- common/csv2json.js
+++ common/csv2json.js
@@ -19,10 +19,13 @@ module.exports.csv2json = async function(/*context*/ _, csvData) {
       let content = {};
       console.log('Iterating results data');
       for (const key in results.data) {
+        console.debug('key: ' + key);
         key.split('.').reduce((acc, e, i, arr) => {
-          return (i === arr.length - 1) ?
+          const returnVal = (i === arr.length - 1) ?
             (acc[e.toString()] = results.data[key]) :
             acc[e.toString()] || (acc[e.toString()] = {});
+          console.debug('Transformed data: ' + returnVal);
+          return returnVal;
         }, content);
       }
       console.log('Sending message to queue');
